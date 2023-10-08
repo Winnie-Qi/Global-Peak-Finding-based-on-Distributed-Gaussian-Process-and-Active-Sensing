b3f5ea4cad71d9ed574b7e4f9ed6ad5fab05668c
Weijie Qi <1913997314@qq.com>
2023-10-08 17:45:28 +0800

    0.3_30 reachGoal

--- f.m
+++ f.m
@@ -1,5 +1,7 @@
 function z = f(x, y)
 % The true environment model, which is assuemd to be unknown.
+% x = x-1;
+% y = y-1;
 z = 3*(1-x).^2.*exp(-(x.^2) - (y+1).^2) ... 
    - 10*(x/5 - x.^3 - y.^5).*exp(-x.^2/2-y.^2) ... 
    - 3*exp(-(x+1).^2 - y.^2);
--- findMaxEdge.m
+++ findMaxEdge.m
@@ -1,12 +1,16 @@
-function [goto_x,goto_y] = findMaxEdge(MovingAgent,f,Exploration)
+function [goto_x,goto_y,ExplorationFlag,step] = findMaxEdge(MovingAgent,f,Exploration)
 
 global InputSpace_LUT;
+ExplorationFlag = 1;
+step = 0;
+m = 1;
+M = 8;
 a = size(f,1);
 [~, index_x] = min(abs(MovingAgent(1) - InputSpace_LUT{1})); 
 [~, index_y] = min(abs(MovingAgent(2) - InputSpace_LUT{2}));
 
 if Exploration
-    L = 10; % side length 21    
+    L = 2; % side length 21    
     while true        
         % Calculates the start and end positions of the window
         start_x = max(index_x-L, 1);
@@ -16,19 +20,52 @@ if Exploration
         % Find the maximum value in the window
         window = f(start_x:end_x, start_y:end_y);
         [max_value, max_index] = max(window(:));
+%         disp(max_value)
         [max_index_x, max_index_y] = ind2sub(size(window), max_index);
 %         goto_x = start_x + max_index_x - 1;
 %         goto_y = start_y + max_index_y - 1;
         goto_x = L - max_index_x + 1;
         goto_y = L - max_index_y + 1;
-        if max_value > 1
+        if max_value > M
+            s = sqrt(goto_x^2 + goto_y^2);
+            if s == 0
+                goto_x = 1; goto_y = 1;
+                m = 1;
+            else
+                goto_x = goto_x/s * m;
+                goto_y = goto_y/s * m;
+            end
             break
+        else
+            if L > 6
+                goto_x=0; goto_y=0;
+                ExplorationFlag = 0;
+                break
+            end
         end
-        L = L+10;
+        L = L+1;
+        m = m + 1;
+        M = M - 0.5;        
     end
 else
-end
+    [max_value, max_index] = max(f(:));
+    [max_index_x, max_index_y] = ind2sub(size(f), max_index);
+    disp(max_index_x)
+    disp(max_index_y)
+    disp(max_value)
+    goto_x = max_index_x - index_x + 1;
+    goto_y = max_index_y - index_y + 1;
+    s = sqrt(goto_x^2 + goto_y^2);
+    if s == 0
+        goto_x = 1; goto_y = 1;        
+    else
+        goto_x = goto_x/s;
+        goto_y = goto_y/s;
+    end
+    if s < 11
+        step = floor(s*0.08/0.03);
+    end
         
+end        
 
 end
-
